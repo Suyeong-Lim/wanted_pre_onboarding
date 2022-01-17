fa713c7fa6223373d4377a4fe25cfd8532e39641
SY <oowwo@naver.com>
2022-01-17 14:35:42 +0900

    캐러셀 완료

---
+++ wantedReact/src/Components/Icons.js
@@ -0,0 +1,11 @@
+import PrevButton from "../img/prevBtn.svg";
+import NextButton from "../img/nextBtn.svg";
+
+const Icons = () => {
+  return {
+    PrevButton,
+    NextButton,
+  };
+};
+
+export default Icons;
--- wantedReact/src/Components/TopBanner.jsx
+++ wantedReact/src/Components/TopBanner.jsx
@@ -8,17 +8,18 @@ import React, {
 import styled from "styled-components";
 import { CarouselData } from "./data/CarouselData";
 
-// @Todo : 인피니트 캐러셀 자연스럽게 
-
 function TopBanner() {
   const newCarouselData = [...CarouselData];
   const [stateCarousel, setStateCarousel] = useState(newCarouselData); //앞뒤 엘레멘트 클론용
   const carouselcount = stateCarousel.length; //데이터 개수
 
   const [current, setCurrent] = useState(1);
-  const [translateX, setTranslateX] = useState(0);
 
-  const SlideListRef = useRef();
+  const windowWidth = window.innerWidth || document.body.clientWidth;
+  const cardWidth = 1060 + 24; //(width + margin)
+  const leftMargin = (windowWidth - cardWidth) / 2;
+
+  const translateX = cardWidth * current - leftMargin;
 
   //앞,뒤 캐러셀이미지 복사
   useEffect(() => {
@@ -27,58 +28,35 @@ function TopBanner() {
     carouselWithClones.unshift(
       carouselWithClones[carouselWithClones.length - 1]
     );
+
     carouselWithClones.push(carouselWithClones[1]);
+
     setStateCarousel(carouselWithClones);
-  }, []);
-
-  //첫 렌더링 시 위치 조정
-  useLayoutEffect(() => {
-    console.log(SlideListRef.current.clientWidth);
-
-    setTranslateX(SlideListRef.current.clientWidth * current + 1524 / 2);
   }, []);
 
   const ClickHandler = useCallback((mode) => {
     if (mode === "prev") {
       console.log(current);
-      if (current < 1) {
+      if (current <= 1) {
         //prev값이 음수가 되려고 하면
-        setTranslateX(SlideListRef.current.clientWidth * current + 1524 / 2);
         setCurrent(carouselcount - 2); //마지막 13 번째 캐러셀 이미지로
       } else {
-        setTranslateX(SlideListRef.current.clientWidth * current + 1524 / 2);
         setCurrent((old) => --old);
       }
     } else if (mode === "next") {
       if (current >= carouselcount - 2) {
         //next 횟수가 캐러셀 길이보다 커지면
-        setTranslateX(SlideListRef.current.clientWidth * current + 1524 / 2);
         setCurrent(1);
       } else {
         //next 실행
         setCurrent((old) => ++old);
-        setTranslateX(SlideListRef.current.clientWidth * current + 1524 / 2);
       }
     }
   });
 
-  //인피니트 캐러셀을 자연스럽게만들기
-  useEffect(() => {
-    const transitionEnd = () => {
-      if (current >= CarouselData.length - 1) {
-      }
-      if (current <= 1) {
-        SlideListRef.current.style.trasition = "none";
-      }
-    };
-    //트랜지션 멈췄다가 다시 시작하게하기? 이미지가 마지막에 도달했으면 현재 슬라이드 맨처음으로 돌리고 이미지 갯수보다 넘어가면
-    //마지막 으로 돌아가게끔.
-    document.addEventListener("transitionend", transitionEnd);
+  console.log("current", current);
 
-    return () => {
-      document.removeEventListener("transitionend", transitionEnd);
-    };
-  }, [current, translateX]);
+  console.log("carouselcount", carouselcount);
 
   return (
     <Main>
@@ -86,16 +64,19 @@ function TopBanner() {
         <SlderTrack
           style={{
             width: "42009px",
-            transition: ".5s ease",
             transform: `translateX(${-translateX}px)`,
+            transition:
+              current == 1 || current >= carouselcount - 2
+                ? "none"
+                : "500ms all ease-in-out",
           }}
         >
           <SlidesContainer>
             {stateCarousel.map((slider, id) => {
               return (
-                <TestDiv ref={SlideListRef}>
-                  <TestImg src={slider.img} key={id}></TestImg>
-                </TestDiv>
+                <SlideItem>
+                  <SlideImg src={slider.img} key={id}></SlideImg>
+                </SlideItem>
               );
             })}
           </SlidesContainer>
@@ -110,12 +91,12 @@ const SlidesContainer = styled.div`
   display: flex;
   width: 100%;
 `;
-const TestDiv = styled.div`
+const SlideItem = styled.div`
   background-color: whitesmoke;
   position: relative;
   padding: 0 12px;
 `;
-const TestImg = styled.img`
+const SlideImg = styled.img`
   background-color: green;
   border-radius: 4px;
 `;
@@ -125,22 +106,26 @@ const Main = styled.main`
 
 const TopBannerWrapper = styled.div`
   position: relative;
-  /* overflow: hidden; */
+  overflow: hidden;
   background-color: pink;
 `;
 
 const SlderTrack = styled.div`
   position: relative;
-
   background-color: blanchedalmond;
 `;
 
 const TopBannerBtn = styled.div`
   display: flex;
-  background-color: orange;
-  margin: 20px;
-  width: 100px;
-  padding: 20px;
+  background-color: #fff;
+  border-radius: 15px;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 143px;
+  width: 30px;
+  height: 60px;
+  opacity: 0.5;
   cursor: pointer;
 `;
 export default TopBanner;
